d9c2c3c7b876a5affc556a394750c34861ff8aaf
Tomer <tomerh@gmail.com>
2025-01-22 00:15:17 +0200

    fix API to actually send the UDP packet...

--- firmware/esp32-5-sliders-3-buttons/lib/api/volume_controller_api.cc
+++ firmware/esp32-5-sliders-3-buttons/lib/api/volume_controller_api.cc
@@ -8,10 +8,10 @@ namespace lib {
 namespace api {
 
 void VolumeControllerApi::sendUdpData(const std::string& data) {
-  // this->_udp_client.beginPacket(this->_server_ip, this->_server_port);
+  this->_udp_client.beginPacket(this->_server_ip, this->_server_port);
   Serial.println(data.c_str());
-  // this->_udp_client.print(data.c_str());
-  // this->_udp_client.endPacket();
+  this->_udp_client.print(data.c_str());
+  this->_udp_client.endPacket();
 }
 
 }  // namespace api
