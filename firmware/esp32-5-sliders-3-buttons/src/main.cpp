c97dbac822a156ae410cb2e6f3a4fc08bb7d4028
Tomer <tomerh@gmail.com>
2025-04-11 10:27:53 +0300

    Add a visual indication that the ESP finished setup.

--- firmware/esp32-5-sliders-3-buttons/src/main.cpp
+++ firmware/esp32-5-sliders-3-buttons/src/main.cpp
@@ -43,6 +43,34 @@ std::vector<MuteButton *> *mute_buttons = nullptr;
 std::vector<Slider *> *sliders = nullptr;
 AudioDeviceSelector *audio_device_selector = nullptr;
 
+void blinkLeds(int ledPin1, int ledPin2, int ledPin3, int delayMs = 600) {
+  // Store the current output states of the pins
+  int previousState1 = digitalRead(ledPin1);
+  int previousState2 = digitalRead(ledPin2);
+  int previousState3 = digitalRead(ledPin3);
+
+  // pinMode(ledPin1, OUTPUT);
+  // pinMode(ledPin2, OUTPUT);
+  // pinMode(ledPin3, OUTPUT);
+
+  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  digitalWrite(ledPin3, HIGH);
+  delay(delayMs);
+
+  digitalWrite(ledPin1, LOW);
+  delay(delayMs);
+  digitalWrite(ledPin2, LOW);
+  delay(delayMs);
+  digitalWrite(ledPin3, LOW);
+  delay(delayMs);
+
+  // Restore the previous output states
+  digitalWrite(ledPin1, previousState1);
+  digitalWrite(ledPin2, previousState2);
+  digitalWrite(ledPin3, previousState3);
+}
+
 void setupWifi() {
   Serial.println("Starting");
   WiFi.begin(ssid, password);
@@ -98,6 +126,10 @@ void setup() {
   Serial.println("Audio device selector button initialized!");
 
   Serial.println("Initialization complete!");
+
+  // Visually indicate that the system is ready.
+  blinkLeds(MUTE_BUTTON_0_LED_PIN, MUTE_BUTTON_1_LED_PIN,
+            AUDIO_DEVICE_SELECTOR_BUTTON_DEV_1_LED_PIN);
 }
 
 void loop() {
